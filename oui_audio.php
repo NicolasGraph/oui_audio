0d5a23b3f00ae67c1883dbaf42091eaa25bbbf4a
Nicolas Morand <contact@nicolasmorand.com>
2018-08-14 15:21:03 +0200

    Holydays updates
    Mainly related to NicolasGraph/oui_provider@c7b78cca1785790ee65ee19409fd86f408283a76

--- oui_audio.php
+++ oui_audio.php
@@ -5,7 +5,7 @@
  * a oui_player v2+ extension to easily create
  * HTML5 customizable video and audio players in Textpattern CMS.
  *
- * https://github.com/NicolasGraph/oui_player_html
+ * https://github.com/NicolasGraph/oui_audio
  *
  * Copyright (C) 2018 Nicolas Morand
  *
@@ -31,190 +31,192 @@
  * @package Oui\Player
  */
 
-namespace Oui {
+namespace Oui;
 
-    if (class_exists('Oui\Provider')) {
+if (class_exists('Oui\Provider')) {
 
-        class Audio extends Provider
+    class Audio extends Provider
+    {
+        protected static $srcBase = '';
+        protected static $srcGlue = ' ';
+        protected static $iniDims = array(
+            'width' => '',
+        );
+        protected static $iniParams = array(
+            'autoplay' => array(
+                'default' => '0',
+                'valid'   => array('0', '1'),
+            ),
+            'controls' => array(
+                'default' => '0',
+                'valid'   => array('0', '1'),
+            ),
+            'loop'     => array(
+                'default' => '0',
+                'valid'   => array('0', '1'),
+            ),
+            'muted'    => array(
+                'default' => '0',
+                'valid'   => array('0', '1'),
+            ),
+            'preload'  => array(
+                'default' => 'auto',
+                'valid'   => array('none', 'metadata', 'auto'),
+            ),
+            'volume'   => array(
+                'default' => '',
+                'valid'   => 'number',
+            ),
+        );
+        protected static $mediaType = 'audio';
+        protected static $mediaPatterns = array(
+            'filename' => array(
+                'scheme' => '#^((?!(http|https)://(www\.)?)\S+\.(mp3|ogg|oga|wav|aac|flac))$#i',
+                'id'     => '1',
+            ),
+            'url' => array(
+                'scheme' => '#^((https?:\/\/(www.)?)\S+\.(mp3|ogg|oga|wav|aac|flac))$#i',
+                'id'     => '1',
+            ),
+        );
+        protected static $mediaMimeTypes = array(
+            'mp3'  => 'audio/mp3',
+            'ogg'  => 'video/ogg',
+            'oga'  => 'video/ogg',
+            'wav'  => 'video/wave',
+            'aac'  => 'audio/aac',
+            'flac' => 'audio/flac',
+        );
+
+        /**
+         * {@inheritdoc}
+         */
+
+        public static function getMediaMimeType($extension)
         {
-            protected static $mediaType = 'audio';
-            protected static $patterns = array(
-                'filename' => array(
-                    'scheme' => '#^((?!(http|https)://(www\.)?)\S+\.(mp3|ogg|oga|wav|aac|flac))$#i',
-                    'id'     => '1',
-                ),
-                'url' => array(
-                    'scheme' => '#^(((http|https):\/\/(www.)?)\S+\.(mp3|ogg|oga|wav|aac|flac))$#i',
-                    'id'     => '1',
-                ),
-            );
-            protected static $mimeTypes = array(
-                'mp3'  => 'audio/mp3',
-                'ogg'  => 'video/ogg',
-                'oga'  => 'video/ogg',
-                'wav'  => 'video/wave',
-                'aac'  => 'audio/aac',
-                'flac' => 'audio/flac',
-            );
-            protected static $src = '';
-            protected static $glue = ' ';
-            protected static $dims = array(
-                'width' => '',
-            );
-            protected static $params = array(
-                'autoplay' => array(
-                    'default' => '0',
-                    'valid'   => array('0', '1'),
-                ),
-                'controls' => array(
-                    'default' => '0',
-                    'valid'   => array('0', '1'),
-                ),
-                'loop'     => array(
-                    'default' => '0',
-                    'valid'   => array('0', '1'),
-                ),
-                'muted'    => array(
-                    'default' => '0',
-                    'valid'   => array('0', '1'),
-                ),
-                'preload'  => array(
-                    'default' => 'auto',
-                    'valid'   => array('none', 'metadata', 'auto'),
-                ),
-                'volume'   => array(
-                    'default' => '',
-                    'valid'   => 'number',
-                ),
-            );
+            return static::$mediaMimeTypes[$extension];
+        }
 
-            /**
-             * {@inheritdoc}
-             */
+        /**
+         * {@inheritdoc}
+         */
 
-            public static function getMimeType($extension)
-            {
-                return static::$mimeTypes[$extension];
-            }
+        public function getParams()
+        {
+            $params = array();
 
-            /**
-             * {@inheritdoc}
-             */
+            foreach (self::getIniParams() as $param => $infos) {
+                $pref = $this->getPref($param);
+                $default = $infos['default'];
+                $value = isset($this->config[$param]) ? $this->config[$param] : '';
 
-            public function getPlayerParams()
-            {
-                $params = array();
-
-                foreach (self::getParams() as $param => $infos) {
-                    $pref = get_pref(Player::getPlugin() . '_' . self::getProvider() . '_' . $param);
-                    $default = $infos['default'];
-                    $value = isset($this->config[$param]) ? $this->config[$param] : '';
-
-                    // Add attributes values in use or modified prefs values as player parameters.
-                    if ($value === '' && $pref !== $default) {
-                        if ($infos['valid'] === array('0', '1')) {
-                            $params[] = $param;
-                        } else {
-                            $params[] = $param . '="' . $pref . '"';
-                        }
-                    } elseif ($value !== '') {
-                        $validArray = is_array($infos['valid']) ? $infos['valid'] : '';
-
-                        if ($validArray && !in_array($value, $validArray)) {
-                            trigger_error('Unknown attribute value for "' . $param . '". Valid values are: "' . implode('", "', $validArray) . '".');
-                        } if ($infos['valid'] === array('0', '1')) {
-                            $params[] = $param;
-                        } else {
-                            $params[] = $param . '="' . $value . '"';
-                        }
-                    }
-                }
-
-                return $params;
-            }
-
-            /**
-             * Get the player code
-             */
-
-            public function getSources()
-            {
-                $infos = $this->getInfos();
-
-                $sources = array();
-
-                foreach ($infos as $play => $info) {
-                    extract($info);
-
-                    if ($type === 'url') {
-                        $sources[] = $play;
+                // Add attributes values in use or modified prefs values as player parameters.
+                if ($value === '' && $pref !== $default) {
+                    if ($infos['valid'] === array('0', '1')) {
+                        $params[$param] = true;
                     } else {
-                        if ($type === 'id') {
-                            $where = 'id = '.intval($play).' and created <= '.now('created');
-                        } elseif ($type === 'filename') {
-                            $where = "filename = '".doSlash($play)."' and created <= ".now('created');
-                        }
+                        $params[$param] = $pref;
+                    }
+                } elseif ($value !== '') {
+                    $validArray = is_array($infos['valid']) ? $infos['valid'] : '';
 
-                        $file = fileDownloadFetchInfo($where);
-
-                        if ($file) {
-                            $sources[] = filedownloadurl($file['id'], $file['filename']);
-                        } else {
-                            trigger_error('Unknown file to play:"' . $play . '"');
-                        }
+                    if ($validArray && !in_array($value, $validArray)) {
+                        trigger_error('Unknown attribute value for "' . $param . '". Valid values are: "' . implode('", "', $validArray) . '".');
+                    } if ($infos['valid'] === array('0', '1')) {
+                        $params[$param] = true;
+                    } else {
+                        $params[$param] =  $value;
                     }
                 }
-
-                return $sources;
             }
 
-            /**
-             * {@inheritdoc}
-             */
+            return $params;
+        }
 
-            public function getPlayer($wraptag = null, $class = null)
-            {
-                if ($sources = $this->getSources()) {
-                    $src = $sources[0];
+        /**
+         * Get the player code
+         */
 
-                    unset($sources[0]);
+        protected function resetSrcGlue($media)
+        {
+            extract($this->mediaInfos[$media]);
+            isset($pattern) ?: $pattern = 'id';
 
-                    $sourcesStr = array();
-
-                    foreach ($sources as $source) {
-                        $sourcesStr[] = '<source src="' . $source . '" type="' . self::getMimeType(pathinfo($source, PATHINFO_EXTENSION)). '">';
-                    }
-
-                    $params = $this->getPlayerParams();
-                    $dims = $this->getSize();
-
-                    extract($dims);
-
-                    $style = '';
-
-                    if (!empty($width)) {
-                        is_string($width) ?: $width .= 'px';
-                        $style = ' style="width:' . $width . '"';
-                    }
-
-                    $player = sprintf(
-                        '<audio src="%s"%s%s>%s%s</audio>',
-                        $src,
-                        $style,
-                        (empty($params) ? '' : ' ' . implode(self::getGlue(), $params)),
-                        ($sourcesStr ? n . implode(n, $sourcesStr) : ''),
-                        n . gtxt(
-                            'oui_player_html_player_not_supported',
-                            array(
-                                '{player}' => '<audio>',
-                                '{src}'    => $src,
-                                '{file}'   => basename($src),
-                            )
-                        ) . n
-                    );
-
-                    return ($wraptag) ? doTag($player, $wraptag, $class) : $player;
+            if ($pattern === 'url') {
+                $sources[] = $media;
+            } else {
+                if ($pattern === 'id') {
+                    $where = 'id = '.intval($media).' and created <= '.now('created');
+                } elseif ($pattern === 'filename') {
+                    $where = "filename = '".doSlash($media)."' and created <= ".now('created');
                 }
+
+                $file = fileDownloadFetchInfo($where);
+
+                if ($file) {
+                    $this->mediaInfos[$media] = array(
+                        'id'  => $file['id'],
+                        'uri' => filedownloadurl($file['id'], $file['filename'])
+                    );
+                } else {
+                    trigger_error('Unknown file to play:"' . $media . '"');
+                }
+            }
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+
+        public function getHTML()
+        {
+            if ($sources = $this->getMediaInfos()) {
+                $src = array_shift($sources)['uri'];
+                $sourcesStr = array();
+
+                foreach ($sources as $source) {
+                    $sourcesStr[] = '<source src="' . $source['uri'] . '" type="' . self::getMediaMimeType(pathinfo($source['uri'], PATHINFO_EXTENSION)). '">';
+                }
+
+                $paramsStr = '';
+
+                foreach ($this->getParams() as $param => $value) {
+                    $paramsStr .= self::getSrcGlue() . ($value === true ? $param : $param . '=' . $value);
+                }
+
+                $dims = $this->getDims();
+
+                extract($dims);
+
+                $style = '';
+
+                if (!empty($width)) {
+                    is_string($width) ?: $width .= 'px';
+                    $style = ' style="width:' . $width . '"';
+                }
+
+                $player = sprintf(
+                    '<audio src="%s"%s%s>%s%s</audio>',
+                    $src,
+                    $style,
+                    $paramsStr,
+                    ($sourcesStr ? n . implode(n, $sourcesStr) : ''),
+                    n . gtxt(
+                        'oui_player_html_player_not_supported',
+                        array(
+                            '{player}' => '<audio>',
+                            '{src}'    => $src,
+                            '{file}'   => basename($src),
+                        )
+                    ) . n
+                );
+
+                list($wraptag, $class) = $this->getWrap();
+                list($label, $labeltag) = $this->getLabel();
+
+                $wraptag ? $player = n . $player . n : '';
+
+                return doLabel($label, $labeltag) . n . doTag($player, $wraptag, $class);
             }
         }
     }
